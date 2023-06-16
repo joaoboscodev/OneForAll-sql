a1a6c084c5fff0751692b5ab821d50e68dc0a808
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-06-16 11:18:22 -0300

    feat: req 06, fix req 01

---
+++ challenges/desafio6.sql
@@ -0,0 +1,7 @@
+SELECT 
+  ROUND(MIN(p.plan_price), 2) AS faturamento_minimo,
+  ROUND(MAX(p.plan_price), 2) AS faturamento_maximo,
+  ROUND(AVG(p.plan_price), 2) AS faturamento_medio,
+  ROUND(SUM(p.plan_price), 2) AS faturamento_total
+FROM SpotifyClone.users u
+INNER JOIN SpotifyClone.plans p ON u.plan_id = p.plan_id;
